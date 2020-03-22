fb979977c0c89c01d9ed616d3c8accd224eeadaf
joyson d. Souza <joyson.dsouza@accolite.com>
2020-03-22 23:14:00 +0530

    Removing cors from controller

--- src/main/java/com/todo/app/controller/AuthController.java
+++ src/main/java/com/todo/app/controller/AuthController.java
@@ -23,7 +23,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
